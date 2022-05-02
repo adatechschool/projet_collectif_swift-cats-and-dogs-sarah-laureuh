de5831bae5b69dfda04a3ea7b7a6887dc841c8fb
Sarah Watremet <sarah.watremet@gmail.com>
2022-05-02 10:34:35 +0200

    added .gitattributes file to avoid merge issues

---
+++ Hawaii_surf_school/Hawaii_surf_school/Hawaii_surf_schoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hawaii_surf_schoolApp.swift
+//  Hawaii_surf_school
+//
+//  Created by Sarah Watremet on 02/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Hawaii_surf_schoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
