67a8015f0e0438c096827daa0242606f8122218b
Sarah Watremet <sarah.watremet@gmail.com>
2022-05-23 10:03:15 +0200

    clean code by deleting some useless comments

--- Hawaii_surf_school/Hawaii_surf_school/Views/Helpers/RectangleImage.swift
+++ Hawaii_surf_school/Hawaii_surf_school/Views/Helpers/RectangleImage.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct RectangleImage: View {
     var surfSpot: SurfSpot
-    //var image: Image
     var body: some View {
         
         AsyncImage(url: URL(string: surfSpot.records[0].photos))
